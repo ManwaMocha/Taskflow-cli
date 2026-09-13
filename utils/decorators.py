110138696d546f98a3c848f2688db325c961003b
mocha <mocha4pf@gmail.com>
2026-09-13 15:34:46 +0300

    feat: add role authorization decorator

--- utils/decorators.py
+++ utils/decorators.py
@@ -13,3 +13,20 @@ def login_required(function):
         return function(self, *args, **kwargs)
 
     return wrapper
+def role_required(required_role):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(self, *args, **kwargs):
+            if self.current_user is None:
+                raise PermissionError("You must log in first.")
+
+            if self.current_user.role != required_role:
+                raise PermissionError(
+                    f"Only {required_role}s can perform this action."
+                )
+
+            return function(self, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
